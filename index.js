f54cb28708bf21b0d89002e337e7ed97e012671b
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-06-26 19:02:14 +0300

    Dicee

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//1.image 1-6
 var randomNumber1 = Math.floor(Math.random() * 6 ) + 1; //1-6 give me
 
 var randomDiceİmage = "dice" + randomNumber1 + ".png";
@@ -8,6 +9,8 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", RandomİmageSource1);
 
+
+//2.image 1-6 
 var randomNumber2 = Math.floor(Math.random() * 6 ) + 1; //1-6 give me
 
 var randomDiceİmage = "dice" + randomNumber2 + ".png";
@@ -17,3 +20,15 @@ var RandomİmageSource2 = "images/" + randomDiceİmage;
 var image1 = document.querySelectorAll("img")[1];
 
 image1.setAttribute("src", RandomİmageSource1);
+
+
+//İf Player Wins
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! ";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
